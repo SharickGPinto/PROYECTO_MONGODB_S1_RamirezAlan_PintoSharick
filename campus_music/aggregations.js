041a0cebc32de7cbeb444be42e2e05f5e1599cb5
SharickGPinto <pinto702rodri@gmail.com>
2025-10-25 15:05:53 -0500

    feat: :sparkles: creating the mongo and its collections

---
+++ campus_music/aggregations.js
@@ -0,0 +1,27 @@
+// CREAR LA BASE DE DATOS
+use campusMusic;
+
+// CREAR LAS COLECCIONES //
+db.createCollection("Usuario");
+
+db.createCollection("Estudiantes");
+
+db.createCollection("Empleados");
+
+db.createCollection("Administradores");
+
+db.createCollection("Cursos");
+
+db.createCollection("Inscripcion");
+
+db.createCollection("Instrumentos");
+
+db.createCollection("Empleados");
+
+db.createCollection("Sede");
+
+db.createCollection("Especialidad");
+
+db.createCollection("ReservaInstrumento");
+
+
